5839bb660ed76a4474b32e0859297af01befb70e
Alex <alex@localhost.localdomain>
2017-09-11 16:57:45 -0600

    added the UDP example from the slides

---
+++ exUDP.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import socket
+
+#SERVER
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('127.0.0.1',5000))
+
+#CLIENT
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.sendto(bytes('hello'),('192.168.124.255',5000))
+
+#SERVER
+data, addr = s.recvfrom(BUFFER_SIZE)
+
